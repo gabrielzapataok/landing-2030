a86a254cfdaacefb7ea7d39d82d385552948d5cf
gabrielzapata <gaby.fz.58@gmail.com>
2021-12-27 15:21:18 -0300

    corrección parallax

--- Scripts/parallax.js
+++ Scripts/parallax.js
@@ -1,10 +1,8 @@
-let bg1, bg2
-window.addEventListener('scroll',function parallex(){
-
-    bg1 = document.getElementById('bg1')
-    bg1.style.top = `${window.pageYOffset * .6}px`
-
-    bg2 = document.getElementById('bg2')
-    bg2.style.top = `${window.pageYOffset * .4 - 500}px`
-
-});
+window.onload = function(){
+    let bg1 = document.getElementById('bg1')
+    let bg2 = document.getElementById('bg2')
+    window.addEventListener('scroll',function parallex(){
+        bg1.style.top = `${window.pageYOffset * .6}px`
+        bg2.style.top = `${window.pageYOffset * .4 - 500}px`
+    });
+}
